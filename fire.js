29839a159ae31e7b1df7280c6b33e9b36ca3fcba
Matias Moretti <matiassmoretti@gmail.com>
2023-06-16 21:37:17 -0300

    Número da célula no canto superior direito

--- fire.js
+++ fire.js
@@ -1,6 +1,6 @@
 const firePixelsArray = []
-const fireWidth = 2
-const fireHeight = 3
+const fireWidth = 10
+const fireHeight = 10
 
 function start(){
     createFireDataStructure()
@@ -30,7 +30,7 @@ function renderFire() {
             const pixelIndex = column + ( fireWidth * row)
             
             html += '<td>'
-            html += pixelIndex
+            html += `<div class="pixel-index">${pixelIndex}</div>`
             html += '</td>'            
 
         }
